8c60b85a7fb500ab7cac1e4735e9567096c14a22
Mikkel Nielsen <mikkel1510@live.dk>
2026-07-09 23:41:12 +0200

    Initial SoMe profile

--- app/src/main/java/com/example/exercises/MainActivity.kt
+++ app/src/main/java/com/example/exercises/MainActivity.kt
@@ -4,13 +4,33 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.exercises.ui.theme.ExercisesTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,7 +40,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             ExercisesTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
+                    Profile(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
@@ -31,17 +51,49 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Profile(name: String, modifier: Modifier = Modifier) {
+    Column(
+        Modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.spacedBy(5.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = name,
+            fontSize = 30.sp
+        )
+        Image(
+            painter = painterResource(id = R.drawable.fella),
+            contentDescription = "Feller",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .clip(CircleShape)
+                .size(200.dp)
+                .border(1.dp, Color.Blue, CircleShape)
+        )
+        var addFriend by remember { mutableStateOf(true) }
+        Button(
+            onClick = { addFriend = !addFriend; },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = if (addFriend) Color.Blue else Color.Red,
+                contentColor = Color.White
+            ),
+        ) {
+            Text(
+                text = if (addFriend) "Add Friend" else "Remove Friend",
+                fontSize = 20.sp,
+                modifier = Modifier
+                    .padding(5.dp)
+            )
+        }
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun ProfilePreview() {
     ExercisesTheme {
-        Greeting("Android")
+        Profile("Steve Bobby")
     }
 }
