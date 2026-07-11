89d3a595a8459e6f6f593aaa7277860367fdc13e
Mikkel Nielsen <mikkel1510@live.dk>
2026-07-12 00:17:56 +0200

    Group page using rememberSaveable

--- app/src/main/java/com/example/exercises/MainActivity.kt
+++ app/src/main/java/com/example/exercises/MainActivity.kt
@@ -1,16 +1,38 @@
 package com.example.exercises
 
 import android.os.Bundle
+import android.widget.GridLayout
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.exercises.ui.theme.ExercisesTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +42,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             ExercisesTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    Group(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +52,77 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun Group(modifier: Modifier = Modifier) {
+    Column(
         modifier = modifier
-    )
+            .fillMaxSize()
+            .padding(10.dp),
+        verticalArrangement = Arrangement.spacedBy(10.dp)
+    ) {
+        val members = rememberSaveable { mutableStateListOf<String>() }
+        var query by rememberSaveable { mutableStateOf("") }
+        Row(
+            Modifier
+                .height(50.dp)
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.spacedBy(5.dp)
+        ) {
+            TextField(
+                value = query,
+                onValueChange = {query = it},
+                modifier = Modifier.fillMaxHeight()
+            )
+            Button(
+                onClick = { members.add(query); query = "" },
+                modifier = Modifier.fillMaxHeight()
+            ) {
+                Text("Add")
+            }
+        }
+        Members(members = members, onRemove = { member -> members.removeAt(member) })
+    }
+}
+
+@Composable
+fun Members(members: List<String>, onRemove: (Int) -> Unit){
+    Column(
+        Modifier
+            .padding(5.dp),
+        verticalArrangement = Arrangement.spacedBy(10.dp)
+    ) {
+        members.forEach { member ->
+            Row(
+                Modifier
+                    .height(60.dp)
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(12.dp))
+                    .background(Color.LightGray)
+                    .padding(10.dp),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(member, fontSize = 20.sp)
+                Button(onClick = { onRemove(members.indexOf(member)) }, colors = ButtonDefaults.buttonColors(containerColor = Color.Red)) {
+                    Text(text = "Remove", fontSize = 20.sp)
+                }
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun GroupPreview() {
     ExercisesTheme {
-        Greeting("Android")
+        Group()
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MembersPreview(){
+    val members = rememberSaveable { mutableStateListOf<String>() }
+    members.add("Pop")
+    members.add("Pete")
+    Members(members = members, onRemove = {})
 }
