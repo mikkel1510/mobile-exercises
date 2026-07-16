d44273b70a8799d387f17de9619286b92e6119d5
Mikkel Nielsen <mikkel1510@live.dk>
2026-07-16 20:00:38 +0200

    Added loading screen to Cars

--- app/src/main/java/com/example/exercises/pages/Cars.kt
+++ app/src/main/java/com/example/exercises/pages/Cars.kt
@@ -3,14 +3,17 @@ package com.example.exercises.pages
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -18,6 +21,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -28,6 +32,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.exercises.R
 import com.example.exercises.RestAPI.CarService.Car
@@ -56,12 +61,25 @@ fun Cars(onBackPress: () -> Unit, carVM: CarViewModel = viewModel()){
         Column(
             Modifier.padding(innerPadding),
         ) {
-            Column(
-                Modifier.padding(10.dp),
-                verticalArrangement = Arrangement.spacedBy(15.dp)
-            ) {
-                carVM.cars.forEach { car ->
-                    Car(car)
+            val isLoading by carVM.isLoading.collectAsStateWithLifecycle()
+
+            if (isLoading){
+                Box(
+                    Modifier.fillMaxSize(),
+                ){
+                    CircularProgressIndicator(
+                        Modifier
+                            .size(100.dp)
+                            .align(Alignment.Center))
+                }
+            } else {
+                Column(
+                    Modifier.padding(10.dp),
+                    verticalArrangement = Arrangement.spacedBy(15.dp)
+                ) {
+                    carVM.cars.forEach { car ->
+                        Car(car)
+                    }
                 }
             }
         }
@@ -106,6 +124,7 @@ fun CarsPreview() {
     )
     val carVM = CarViewModel()
     carVM.cars.addAll(previewCars)
+    carVM.isLoading.value = false
 
     Cars({}, carVM)
 }
--- app/src/main/java/com/example/exercises/viewmodels/CarViewModel.kt
+++ app/src/main/java/com/example/exercises/viewmodels/CarViewModel.kt
@@ -1,16 +1,22 @@
 package com.example.exercises.viewmodels
 
+import android.util.Log
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.exercises.RestAPI.CarService.Car
 import com.example.exercises.RestAPI.RetrofitClient
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
 
 class CarViewModel: ViewModel() {
 
     private val retrofitClient = RetrofitClient()
     val cars = mutableStateListOf<Car>()
+
+    val isLoading = MutableStateFlow(false)
 
     init {
         fetchCars()
@@ -19,8 +25,14 @@ class CarViewModel: ViewModel() {
     fun fetchCars() {
         cars.clear()
         viewModelScope.launch {
-            val response = retrofitClient.api.getCars()
-            cars.addAll(response)
+            isLoading.value = true
+            try {
+                val response = retrofitClient.api.getCars()
+                cars.addAll(response)
+                isLoading.value = false
+            } catch (e: Exception){
+                Log.e("CarViewModel", "Could not fetch cars", e)
+            }
         }
     }
 }
